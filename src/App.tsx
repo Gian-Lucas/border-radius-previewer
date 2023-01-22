bfed34680db2012bdcaa85f25bcba75d8e85f361
Gian Lucas <lgian584@gmail.com>
2023-01-21 23:28:24 -0300

    challenge finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,96 @@
+import { useState } from "react";
 import "./App.css";
 import "./global.css";
 
+import GitHubImg from "./assets/github.svg";
+
 export function App() {
+  const [topLeft, setTopLeft] = useState(0);
+  const [topRight, setTopRight] = useState(0);
+  const [bottomLeft, setBottomLeft] = useState(0);
+  const [bottomRight, setBottomRight] = useState(0);
+
   return (
-    <main className="container">
-      <div className="title">
-        <h1>[Previewer]</h1>
-        <h1>border-radius:</h1>
-      </div>
+    <>
+      <a
+        className="repo-link"
+        href="https://github.com/Gian-Lucas/border-radius-previewer"
+        target="_blank"
+        title="Go to github repository"
+      >
+        <img src={GitHubImg} alt="GitHub" />
+      </a>
 
-      <div className="code-container">
-        <div className="code-result">border-radius: 50px 0 0 0;</div>
+      <main className="container">
+        <div className="title">
+          <h1>[Previewer]</h1>
+          <h1>border-radius:</h1>
+        </div>
 
-        <div className="border-viewer">
-          <div className="inputs">
-            <div>
-              <label htmlFor="top-left">Top Left</label>
-              <input id="top-left" type="number" />
-            </div>
-            <div>
-              <label htmlFor="top-right">Top Right</label>
-              <input id="top-right" type="number" />
-            </div>
+        <div className="code-container">
+          <div className="code-result">
+            border-radius: {topLeft !== 0 ? `${topLeft}px ` : `${topLeft} `}
+            {topRight !== 0 ? `${topRight}px ` : `${topRight} `}
+            {bottomLeft !== 0 ? `${bottomLeft}px ` : `${bottomLeft} `}
+            {bottomRight !== 0 ? `${bottomRight}px` : bottomRight};
           </div>
 
-          <div className="border-result"></div>
-
-          <div className="inputs">
-            <div>
-              <label htmlFor="bottom-left">Bottom Left</label>
-              <input id="bottom-left" type="number" />
+          <div className="border-viewer">
+            <div className="inputs">
+              <div>
+                <label htmlFor="top-left">Top Left</label>
+                <input
+                  min={0}
+                  value={topLeft}
+                  onChange={(e) => setTopLeft(Number(e.target.value))}
+                  id="top-left"
+                  type="number"
+                />
+              </div>
+              <div>
+                <label htmlFor="top-right">Top Right</label>
+                <input
+                  min={0}
+                  value={topRight}
+                  onChange={(e) => setTopRight(Number(e.target.value))}
+                  id="top-right"
+                  type="number"
+                />
+              </div>
             </div>
-            <div>
-              <label htmlFor="bottom-right">Bottom Right</label>
-              <input id="bottom-right" type="number" />
+
+            <div
+              className="border-result"
+              style={{
+                borderRadius: `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px`,
+              }}
+            />
+
+            <div className="inputs">
+              <div>
+                <label htmlFor="bottom-left">Bottom Left</label>
+                <input
+                  min={0}
+                  value={bottomLeft}
+                  onChange={(e) => setBottomLeft(Number(e.target.value))}
+                  id="bottom-left"
+                  type="number"
+                />
+              </div>
+              <div>
+                <label htmlFor="bottom-right">Bottom Right</label>
+                <input
+                  min={0}
+                  value={bottomRight}
+                  onChange={(e) => setBottomRight(Number(e.target.value))}
+                  id="bottom-right"
+                  type="number"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
